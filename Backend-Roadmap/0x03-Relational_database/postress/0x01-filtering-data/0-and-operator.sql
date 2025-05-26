9038905b264b3bf192b8bb2c0ce9d8c1a043cf2d
Harvey W. Kis <harveykisiangani@gmail.com>
2025-05-26 22:23:49 +0300

    AND Operator

---
+++ Backend-Roadmap/0x03-Relational_database/postress/0x01-filtering-data/0-and-operator.sql
@@ -0,0 +1,7 @@
+-- And operator
+-- true: true, 't', 'true', 'y', 'yes, '1'
+-- false: false, 'f', 'false', 'n', 'no', '0'
+
+-- returns false since combination of false and true results to false
+SELECT false AND false AS result;
+
