eb71d8aecc4b3c35ad9c7376541f7b2fc711421b
Harvey W. Kis <harveykisiangani@gmail.com>
2025-05-28 21:26:05 +0300

    Between and not between

---
+++ Backend-Roadmap/0x03-Relational_database/postress/0x01-filtering-data/13-not-between.sql
@@ -0,0 +1,10 @@
+-- NOT BETWEEN statement
+SELECT
+	payment_id,
+	amount
+FROM
+	payment
+WHERE
+	payment_id NOT BETWEEN 17503 AND 17505
+ORDER BY
+	payment_id;
---
+++ Backend-Roadmap/0x03-Relational_database/postress/0x01-filtering-data/14-between-date-range.sql
@@ -0,0 +1,13 @@
+-- NOT BETWEEN statement
+SELECT
+	customer_id,
+	payment_id,
+	amount,
+	payment_date
+FROM
+	payment
+WHERE
+	payment_date BETWEEN '2007-02-15' AND '2007-02-20'
+	AND amount > 10
+ORDER BY
+	payment_date;
